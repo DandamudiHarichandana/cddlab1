8a4daba0a6aefc25e69778c08aba72a392386882
Harichandana dandamudi <harichandana.dandamudi@gmail.com>
2021-09-03 11:25:27 +0530

    first commit

---
+++ sum.java
@@ -0,0 +1,7 @@
+public class AddToNumbers{
+       public static void main(String args[]){
+             int num1 = 5,num2 = 15,sum=0;
+              sum = num1+num2;
+              System.out.println("sum of two nums: "+sum);
+}
+}
